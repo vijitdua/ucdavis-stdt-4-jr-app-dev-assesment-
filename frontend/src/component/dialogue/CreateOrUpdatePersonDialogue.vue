57848f6b0b471a6e7732480d9f680f8ac849bec9
vijitdua <82555472+vijitdua@users.noreply.github.com>
2025-03-20 15:46:28 -0700

    create: blank dialog component for person form

---
+++ frontend/src/component/dialogue/CreateOrUpdatePersonDialogue.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { watchEffect, ref } from 'vue';
+import type { Person } from "@/services/personsApiService";
+
+const props = defineProps<{
+  open: boolean;
+  isEditMode?: boolean;
+  personId?: string;
+  personData?: Person;
+  close: () => void;
+}>();
+
+const dialogVisible = ref(false);
+
+watchEffect(() => {
+  dialogVisible.value = props.open;
+});
+
+// call parent close function
+function handleClose() {
+  props.close();
+}
+</script>
+
+<template>
+  <v-dialog v-model="dialogVisible" max-width="600px">
+    <v-card>
+      <v-card-title>
+      </v-card-title>
+      // content
+      <v-card-actions>
+        <v-btn @click="handleClose">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
